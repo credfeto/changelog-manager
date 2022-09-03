470dd4de3d814230cabeab7ba1292be82c37dbe0
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-09-03 10:50:00 +0100

    Added test for the case where it is removing from released section

--- src/Credfeto.ChangeLog.Tests/ChangeLogUpdaterRemoveEntryTests.cs
+++ src/Credfeto.ChangeLog.Tests/ChangeLogUpdaterRemoveEntryTests.cs
@@ -176,4 +176,56 @@ Releases that have at least been deployed to staging, BUT NOT necessarily releas
 
         Assert.Equal(expected.ToLocalEndLine(), actual: result);
     }
+
+    [Fact]
+    public void DoesNotRemoveLineFromReleasedSection()
+    {
+        const string existing = @"# Changelog
+All notable changes to this project will be documented in this file.
+
+<!--
+Please ADD ALL Changes to the UNRELEASED SECTION and not a specific release
+-->
+
+## [Unreleased]
+### Added
+- Do Not Remove Me
+### Fixed
+### Changed
+### Removed
+### Deployment Changes
+
+<!--
+Releases that have at least been deployed to staging, BUT NOT necessarily released to live.  Changes should be moved from [Unreleased] into here as they are merged into the appropriate release branch
+-->
+## [1.0.0]
+### Added
+- Remove Me
+
+## [0.0.0] - Project created";
+
+        string result = ChangeLogUpdater.RemoveEntry(changeLog: existing, type: "Added", message: "Remove Me");
+
+        const string expected = @"# Changelog
+All notable changes to this project will be documented in this file.
+
+<!--
+Please ADD ALL Changes to the UNRELEASED SECTION and not a specific release
+-->
+
+## [Unreleased]
+### Added
+- Do Not Remove Me
+### Fixed
+### Changed
+### Removed
+### Deployment Changes
+
+<!--
+Releases that have at least been deployed to staging, BUT NOT necessarily released to live.  Changes should be moved from [Unreleased] into here as they are merged into the appropriate release branch
+-->
+## [0.0.0] - Project created";
+
+        Assert.Equal(expected.ToLocalEndLine(), actual: result);
+    }
 }
