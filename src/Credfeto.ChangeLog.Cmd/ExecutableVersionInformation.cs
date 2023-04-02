76b6683982af20219f61c31114303cde2c846630
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-04-02 20:21:34 +0100

    Version information

--- src/Credfeto.ChangeLog.Cmd/ExecutableVersionInformation.cs
+++ src/Credfeto.ChangeLog.Cmd/ExecutableVersionInformation.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Credfeto.ChangeLog.Cmd;
 
@@ -7,13 +12,56 @@ internal static class ExecutableVersionInformation
 {
     public static string ProgramVersion()
     {
-        return CommonVersion(typeof(ExecutableVersionInformation));
+        return CommonVersion(typeof(Program));
     }
 
     private static string CommonVersion(Type type)
     {
-        FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(type.Assembly.Location);
+        Assembly assembly = type.Assembly;
+
+        return GetAssemblyFileVersionFile() ?? GetAssemblyFileVersion(assembly) ?? GetAssemblyVersion(assembly);
+    }
+
+    private static string? GetAssemblyFileVersionFile()
+    {
+        IReadOnlyList<string> args = Environment.GetCommandLineArgs();
+
+        if (args.Count == 0)
+        {
+            return null;
+        }
+
+        string location = args[0];
+
+        if (string.IsNullOrWhiteSpace(location) || !File.Exists(location))
+        {
+            return null;
+        }
+
+        FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
 
         return fileVersionInfo.ProductVersion!;
+    }
+
+    private static string? GetAssemblyFileVersion(Assembly assembly)
+    {
+        AssemblyFileVersionAttribute? fvi = assembly.GetCustomAttributes<AssemblyFileVersionAttribute>()
+                                                    .FirstOrDefault();
+
+        return fvi?.Version;
+    }
+
+    private static string GetAssemblyVersion(Assembly assembly)
+    {
+        Console.Write("Finding Assembly Version");
+        Version? assemblyVersion = assembly.GetName()
+                                           .Version;
+
+        if (assemblyVersion == null)
+        {
+            throw new VersionNotFoundException();
+        }
+
+        return assemblyVersion.ToString();
     }
 }
