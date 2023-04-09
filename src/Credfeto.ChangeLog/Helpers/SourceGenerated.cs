cf7405e49f9243c18ca7564ecd0062230ee9bf6d
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-04-09 23:55:56 +0100

    .net 6 compatibility

--- src/Credfeto.ChangeLog/Helpers/SourceGenerated.cs
+++ src/Credfeto.ChangeLog/Helpers/SourceGenerated.cs
@@ -5,33 +5,31 @@ namespace Credfeto.ChangeLog.Helpers;
 #if NET6_0
 internal static class SourceGenerated
 {
-    private static readonly TimeSpan Short = TimeSpan.FromMilliseconds(RegexSettings.TimeoutMilliseconds);
-
     public static Regex RemoveCommentsRegex()
     {
-        return new(pattern: RegexSettings.REMOVE_COMMENTS_REGEX, options: RegexSettings.REMOVE_COMMENTS_OPTIONS, matchTimeout: Short);
+        return new(pattern: RegexSettings.RemoveCommentsRegex, options: RegexSettings.RemoveCommentsOptions, matchTimeout: Net6Compat.Short);
     }
 
     public static Regex VersionHeaderRegex()
     {
-        return new(pattern: RegexSettings.VERSION_HEADER_REGEX, options: RegexSettings.VERSION_HEADER_OPTIONS, matchTimeout: Short);
+        return new(pattern: RegexSettings.VersionHeaderRegex, options: RegexSettings.VersionHeaderOptions, matchTimeout: Net6Compat.Short);
     }
 
     public static Regex GitHunkPositionRegex()
     {
-        return new(pattern: RegexSettings.GIT_HUNK_POSITION_REGEX, options: RegexSettings.GIT_HUNK_POSITION_OPTIONS, matchTimeout: Short);
+        return new(pattern: RegexSettings.GitHunkPositionRegex, options: RegexSettings.GitHunkPositionOptions, matchTimeout: Net6Compat.Short);
     }
 }
 #else
 internal static partial class SourceGenerated
 {
-    [GeneratedRegex(pattern: RegexSettings.REMOVE_COMMENTS_REGEX, options: RegexSettings.REMOVE_COMMENTS_OPTIONS, matchTimeoutMilliseconds: RegexSettings.TimeoutMilliseconds)]
+    [GeneratedRegex(pattern: RegexSettings.RemoveCommentsRegex, options: RegexSettings.RemoveCommentsOptions, matchTimeoutMilliseconds: RegexSettings.TimeoutMilliseconds)]
     public static partial Regex RemoveCommentsRegex();
 
-    [GeneratedRegex(pattern: RegexSettings.VERSION_HEADER_REGEX, options: RegexSettings.VERSION_HEADER_OPTIONS, matchTimeoutMilliseconds: RegexSettings.TimeoutMilliseconds)]
+    [GeneratedRegex(pattern: RegexSettings.VersionHeaderRegex, options: RegexSettings.VersionHeaderOptions, matchTimeoutMilliseconds: RegexSettings.TimeoutMilliseconds)]
     public static partial Regex VersionHeaderRegex();
 
-    [GeneratedRegex(pattern: RegexSettings.GIT_HUNK_POSITION_REGEX, options: RegexSettings.GIT_HUNK_POSITION_OPTIONS, matchTimeoutMilliseconds: RegexSettings.TimeoutMilliseconds)]
+    [GeneratedRegex(pattern: RegexSettings.GitHunkPositionRegex, options: RegexSettings.GitHunkPositionOptions, matchTimeoutMilliseconds: RegexSettings.TimeoutMilliseconds)]
     public static partial Regex GitHunkPositionRegex();
 }
 #endif
