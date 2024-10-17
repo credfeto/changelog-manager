f83ea19540f48a53823a2c1ea1556ab0c907feea
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-10-17 11:57:03 +0100

    Versioning

--- src/Credfeto.ChangeLog.Cmd/ExecutableVersionInformation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Credfeto.ChangeLog.Cmd;
-
-internal static class ExecutableVersionInformation
-{
-    public static string ProgramVersion()
-    {
-        return ThisAssembly.Info.FileVersion;
-    }
-}
