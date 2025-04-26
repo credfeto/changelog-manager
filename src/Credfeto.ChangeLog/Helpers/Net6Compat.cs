19bb793926130384b135c57915f1d25189105b3d
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 11:45:28 +0000

    Cleanup: Net6Compat.cs

--- src/Credfeto.ChangeLog/Helpers/Net6Compat.cs
+++ src/Credfeto.ChangeLog/Helpers/Net6Compat.cs
@@ -3,19 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Credfeto.ChangeLog.Helpers;
 
-[SuppressMessage(
-    category: "ReSharper",
-    checkId: "UnusedType.Global",
-    Justification = "TODO: Review"
-)]
+[SuppressMessage(category: "ReSharper", checkId: "UnusedType.Global", Justification = "TODO: Review")]
 internal static class Net6Compat
 {
-    [SuppressMessage(
-        category: "ReSharper",
-        checkId: "UnusedMember.Global",
-        Justification = "TODO: Review"
-    )]
-    public static readonly TimeSpan Short = TimeSpan.FromMilliseconds(
-        RegexSettings.TimeoutMilliseconds
-    );
+    [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global", Justification = "TODO: Review")]
+    public static readonly TimeSpan Short = TimeSpan.FromMilliseconds(RegexSettings.TimeoutMilliseconds);
 }
